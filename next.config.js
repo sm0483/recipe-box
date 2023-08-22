76d1e1258309b43623a49c1947e9e0e45c108af1
sreerag <msratofficial@gmail.com>
2023-08-22 20:44:56 +0530

    fix:config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
          },
       ],
    },
+   async rewrites() {
+      return [
+         {
+            source: "/api/v1/:path*",
+            destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+         },
+      ];
+   },
 };
 
 module.exports = nextConfig;
